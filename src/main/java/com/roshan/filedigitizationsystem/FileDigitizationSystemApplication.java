630b653ff48498000fa7614b63e407110d3ec6c9
Roshan Nepal <roshan.n9959@gmail.com>
2025-01-31 16:43:54 +0545

    Uploading Project Files

---
+++ src/main/java/com/roshan/filedigitizationsystem/FileDigitizationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.roshan.filedigitizationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDigitizationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileDigitizationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roshan/filedigitizationsystem/FileDigitizationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roshan.filedigitizationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDigitizationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
