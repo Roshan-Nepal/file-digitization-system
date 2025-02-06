13cac4b81da6a0b2b56ed22129746bff3c136481
Roshan Nepal <roshan.n9959@gmail.com>
2025-02-06 22:03:26 +0545

    Remove unused import

--- src/main/java/com/roshan/filedigitizationsystem/exception/NoSuchUserExistsException.java
+++ src/main/java/com/roshan/filedigitizationsystem/exception/NoSuchUserExistsException.java
@@ -1,7 +1,5 @@
 package com.roshan.filedigitizationsystem.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class NoSuchUserExistsException extends RuntimeException {
     public NoSuchUserExistsException() {
