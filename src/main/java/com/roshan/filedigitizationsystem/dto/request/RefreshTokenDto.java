01eb7beb07d779173e5b10802da7186b9096ec64
Roshan Nepal <roshan.n9959@gmail.com>
2025-02-09 19:20:17 +0545

    Add RefreshTokenDto

---
+++ src/main/java/com/roshan/filedigitizationsystem/dto/request/RefreshTokenDto.java
@@ -0,0 +1,12 @@
+package com.roshan.filedigitizationsystem.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class RefreshTokenDto {
+    private String token;
+}
